d161b8d0a8eaa865b245947e729441e4a72b712c
Fernando2706 <43041083+Fernando2706@users.noreply.github.com>
2020-02-20 23:54:51 +0100

    Add files via upload

--- Caracteres/src/WorldOffMind.java
+++ Caracteres/src/WorldOffMind.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.worldoffmind.game;
 
 import java.util.Iterator;
 
@@ -8,22 +8,23 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
 import Attack.Ataque;
 import Attack.AtaqueFuego;
+import Attack.AtaqueFuego.State;
 import personajes.Caracteres;
-import personajes.JugadorPrincipal;
-import personajes.Minotauro;
+import personajes.Players;
+import personajes.Skeletons;
 import personajes.Spider;
+import personajes.Minotauro;
 
 public class WorldOffMind extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
-	JugadorPrincipal jugador;
+	Players jugador;
 	Minotauro minotauro;
 	Spider spider;
 	Array<Ataque> ataques;
@@ -32,13 +33,13 @@ public class WorldOffMind extends Game {
 	public void createAttack(String dire) {
 		AtaqueFuego a= new AtaqueFuego(this, this.jugador.position.x+20, this.jugador.position.y+20);
 		if(dire.equals("izquierda")) {
-			a.state=a.state.LEFT;
+			a.state=State.LEFT;
 		}else if(dire.equals("derecha")) {
-			a.state=a.state.RIGHT;
+			a.state=State.RIGHT;
 		}else if(dire.equals("arriba")) {
-			a.state=a.state.UP;
+			a.state=State.UP;
 		}else if(dire.equals("abajo")){
-			a.state=a.state.DOWN;
+			a.state=State.DOWN;
 		}
 		lastSpell=TimeUtils.nanoTime();
 		ataques.add(a);
@@ -48,13 +49,25 @@ public class WorldOffMind extends Game {
 	public void create () {
 		this.batch=new SpriteBatch();
 		this.font=new BitmapFont();
-		jugador=new JugadorPrincipal(this, 10, 10);
-		minotauro=new Minotauro(this, 500, 500);
-		spider=new Spider(this,"Ice2",200,200);
+		jugador=new Players(this, 10, 10);
+		minotauro=new Minotauro(this, 900, 1000);
+		spider=new Spider(this,"Ice2",1250,1000);
 		ataques=new Array<Ataque>();
 		enemies=new Array<Caracteres>();
 		enemies.add(minotauro);
 		enemies.add(spider);
+		enemies.add(new Spider(this,"Ice2",1250,1000));
+		enemies.add(new Spider(this,"Ice1",1300,1000));
+		enemies.add(new Spider(this,"Ice3",1350,1000));
+		enemies.add(new Spider(this,"Ice4",1400,1000));
+		enemies.add(new Spider(this,"Fire1",1450,1000));
+		enemies.add(new Spider(this,"Golden",1500,1000));
+		enemies.add(new Spider(this,"GoldenPoisson",1550,1000));
+		enemies.add(new Skeletons(this, 1050, 1000, true));
+		enemies.add(new Skeletons(this, 1100, 1000, false));
+		enemies.add(new Skeletons(this, 1150, 1000, false));
+		enemies.add(new Skeletons(this, 1200, 1000, false));
+
 
 	}
 	
@@ -73,11 +86,11 @@ public class WorldOffMind extends Game {
 
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
 			if(jugador.position.x==0.0||tocando==true) {
-				jugador.state=jugador.state.IDLELeft;
+				jugador.state=Players.State.IDLELeft;
 				estado="izquierda";
 			}
 			else {
-				jugador.state=	jugador.state.LEFT;
+				jugador.state=	Players.State.LEFT;
 				jugador.move(new Vector2(-dt, 0));
 				estado="izquierda";
 				atacando=false;
@@ -91,10 +104,10 @@ public class WorldOffMind extends Game {
 
 		else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			if(jugador.position.x==Gdx.graphics.getWidth()-51||tocando==true) {
-				jugador.state=jugador.state.IDLERight;
+				jugador.state=Players.State.IDLERight;
 				estado="derecha";
 			}else {
-				jugador.state=	jugador.state.RIGHT;
+				jugador.state=	Players.State.RIGHT;
 				jugador.move(new Vector2(dt, 0));
 				estado="derecha";
 				atacando=false;
@@ -105,10 +118,10 @@ public class WorldOffMind extends Game {
 
 		else if (Gdx.input.isKeyPressed(Keys.UP)) {
 			if(jugador.position.y==Gdx.graphics.getHeight()-58||tocando==true) {
-				jugador.state=jugador.state.IDLEUp;
+				jugador.state=Players.State.IDLEUp;
 				estado="arriba";
 			}else {
-				jugador.state=	jugador.state.UP;
+				jugador.state=	Players.State.UP;
 				jugador.move(new Vector2(0, dt));
 				estado="arriba";
 				atacando=false;
@@ -119,10 +132,10 @@ public class WorldOffMind extends Game {
 
 		else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
 			if(jugador.position.y==0||tocando==true) {
-				jugador.state=jugador.state.IDLEDown;
+				jugador.state=Players.State.IDLEDown;
 				estado="abajo";
 			}else {
-				jugador.state=	jugador.state.DOWN;
+				jugador.state=	Players.State.DOWN;
 				jugador.move(new Vector2(0, -dt));
 				estado="abajo";
 				atacando=false;
@@ -132,26 +145,26 @@ public class WorldOffMind extends Game {
 		else if(Gdx.input.isKeyPressed(Keys.Q)){
 
 			if(estado.contentEquals("izquierda")) {
-				jugador.state=jugador.state.attackLeft;
+				jugador.state=Players.State.attackLeft;
 				if(TimeUtils.nanoTime()-lastSpell>500000000) {
 					createAttack("izquierda");
 				}
 			
 			}else if(estado.contentEquals("derecha")) {
-				jugador.state=jugador.state.attackRight;
+				jugador.state=Players.State.attackRight;
 				if(TimeUtils.nanoTime()-lastSpell>500000000) {
 					createAttack("derecha");
 				}
 			}
 			else if(estado.contentEquals("arriba")) {				
-				jugador.state=jugador.state.attackUp;
+				jugador.state=Players.State.attackUp;
 				if(TimeUtils.nanoTime()-lastSpell>500000000) {
 					createAttack("arriba");
 
 				}
 			}
 			else if(estado.contentEquals("abajo")) {
-				jugador.state=jugador.state.attackDown;
+				jugador.state=Players.State.attackDown;
 				if(TimeUtils.nanoTime()-lastSpell>500000000) {
 					createAttack("abajo");
 				}
@@ -165,14 +178,14 @@ public class WorldOffMind extends Game {
 		else {
 			atacando=false;
 			if(estado.contentEquals("izquierda")) {
-				jugador.state=jugador.state.IDLELeft;
+				jugador.state=Players.State.IDLELeft;
 			}else if(estado.contentEquals("derecha")) {
-				jugador.state=jugador.state.IDLERight;
+				jugador.state=Players.State.IDLERight;
 			}
 			else if(estado.contentEquals("arriba")) {
-				jugador.state=jugador.state.IDLEUp;
+				jugador.state=Players.State.IDLEUp;
 			}else if(estado.contentEquals("abajo")) {
-				jugador.state=jugador.state.IDLEDown;
+				jugador.state=Players.State.IDLEDown;
 			}
 		}
 
@@ -234,13 +247,14 @@ public class WorldOffMind extends Game {
 		Iterator<Caracteres> iter2=enemies.iterator();
 		while(iter2.hasNext()) {
 			Caracteres a=iter2.next();
+			a.update(dt);
 			if(a.dead==true) {
 				iter2.remove();
 			}
 			
 		}
 		jugador.update(dt);
-		minotauro.update(dt);
+		
 		for(Ataque a: ataques)
 			a.update(dt);
 	}
